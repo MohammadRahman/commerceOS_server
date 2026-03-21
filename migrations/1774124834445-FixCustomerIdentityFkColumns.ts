d714116ae954a98e0204f5d06bd7ab65b7949d33
mohammadrahman <obaidurapu12@gmail.com>
2026-03-21 22:46:55 +0200

    customer_identites migration updates

--- migrations/1774124834445-FixCustomerIdentityFkColumns.ts
+++ migrations/1774124834445-FixCustomerIdentityFkColumns.ts
@@ -11,17 +11,10 @@ export class FixCustomerIdentityFkColumns1774124834445 implements MigrationInter
       `ALTER TABLE "customer_identities" DROP CONSTRAINT "FK_e4781390482956317ada1840bfa"`,
     );
     await queryRunner.query(
-      `ALTER TABLE "orders" DROP CONSTRAINT "FK_e5de51ca888d8b1f5ac25799dd1"`,
-    );
-    await queryRunner.query(
       `ALTER TABLE "customer_identities" DROP COLUMN "customerId"`,
     );
     await queryRunner.query(
       `ALTER TABLE "customer_identities" DROP COLUMN "channelId"`,
-    );
-    await queryRunner.query(`ALTER TABLE "orders" DROP COLUMN "customerId"`);
-    await queryRunner.query(
-      `ALTER TABLE "channels" ADD "name" character varying(200)`,
     );
     await queryRunner.query(
       `ALTER TABLE "payment_provider_catalog" ALTER COLUMN "supported_countries" SET DEFAULT ARRAY['BD']::text[]`,
@@ -63,16 +56,11 @@ export class FixCustomerIdentityFkColumns1774124834445 implements MigrationInter
     await queryRunner.query(
       `ALTER TABLE "payment_provider_catalog" ALTER COLUMN "supported_countries" SET DEFAULT ARRAY['BD']::text[]`,
     );
-    await queryRunner.query(`ALTER TABLE "channels" DROP COLUMN "name"`);
-    await queryRunner.query(`ALTER TABLE "orders" ADD "customerId" uuid`);
     await queryRunner.query(
       `ALTER TABLE "customer_identities" ADD "channelId" uuid`,
     );
     await queryRunner.query(
       `ALTER TABLE "customer_identities" ADD "customerId" uuid`,
-    );
-    await queryRunner.query(
-      `ALTER TABLE "orders" ADD CONSTRAINT "FK_e5de51ca888d8b1f5ac25799dd1" FOREIGN KEY ("customerId") REFERENCES "customers"("id") ON DELETE RESTRICT ON UPDATE NO ACTION`,
     );
     await queryRunner.query(
       `ALTER TABLE "customer_identities" ADD CONSTRAINT "FK_e4781390482956317ada1840bfa" FOREIGN KEY ("channelId") REFERENCES "channels"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
